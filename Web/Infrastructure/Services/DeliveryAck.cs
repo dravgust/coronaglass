4e4e0986dd0ef5046c1e057426dfd4b8307345db
dravgust <dravgust@hotmail.com>
2021-05-15 09:43:56 +0300

    ...

---
+++ Web/Infrastructure/Services/DeliveryAck.cs
@@ -0,0 +1,11 @@
+﻿namespace Web.Infrastructure.Services
+{
+    public class DeliveryAck
+    {
+        public long MessageId { get; }
+        public DeliveryAck(long messageId)
+        {
+            MessageId = messageId;
+        }
+    }
+}
---
+++ Web/Infrastructure/Services/DeliveryEnvelope.cs
@@ -0,0 +1,13 @@
+﻿namespace Web.Infrastructure.Services
+{
+    public class DeliveryEnvelope<TMessage>
+    {
+        public TMessage Message { get; }
+        public long MessageId { get; }
+        public DeliveryEnvelope(TMessage message, long messageId)
+        {
+            Message = message;
+            MessageId = messageId;
+        }
+    }
+}
